f19922c97e207789224962351d6d247ab55024fc
therealcalle <cally.x666@googlemail.com>
2024-11-17 13:19:52 +0100

    Update gpu.sh

--- hyprdots/waybar/modules/gpu.sh
+++ hyprdots/waybar/modules/gpu.sh
@@ -9,7 +9,7 @@ QUERY=$(nvidia-smi --query-gpu=utilization.memory,temperature.gpu --format=csv,n
 
 # Extract data from query
 USAGE=$(echo $QUERY | awk -F, '{print $1}')
-TEMP=$(echo $QUERY | awk -F, '{print $2}')
+TEMP=$(echo $QUERY | awk -F, '{print $2}' | tr -d ' ')
 
 # Output with colored spans
 echo "{\"text\": \"<span color='$GPU_COLOR'>$USAGE%</span> | <span color='$TEMP_COLOR'>$TEMP°C</span>\", \"alt\": \"$USAGE\", \"tooltip\": \"GPU Usage: $USAGE%\\nTemp: $TEMP°C\", \"class\": \"gpu\"}"
